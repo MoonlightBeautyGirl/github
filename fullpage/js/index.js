bbdf4b3d890c8624399990f837e2e3f69aa9931f
安晓 <18610834353@163.com>
2016-11-10 18:06:48 +0800

    fullpage

---
+++ fullpage/js/index.js
@@ -0,0 +1,15 @@
+	$(function(){
+		$('#fullPage').fullpage({
+			navigation:true,
+			navigationPosition:'right',
+			navigationColor:'#ff0000',
+			navigationTooltips:['孤单的不只是背影','就喜欢这样静静的看着你','那一天阳光正好，我穿了喜欢的裙子','转眼秋天，我身边有了你','炎热的夏天头发那么长','你会为谁束起长发','后面的大叔你在看什么！'],
+			slidesNavigation:true,
+			slidesNavPosition:'bottom',
+			continuousVertical:true,
+			showActiveTooltip:true,
+			controlArrows:false,
+			keyboardScrolling:true,
+			verticalCentered:false
+		});
+	})
